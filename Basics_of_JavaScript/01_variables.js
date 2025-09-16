96fce3e9114b2f87124dd55836170d7de60e9d75
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-09-16 17:31:23 +0000

    Variables in JavaScipt

---
+++ Basics_of_JavaScript/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12334
+let emailId = "princebisht12245@gmail.com"
+var emPassword = "Princeewjrt@123.,"
+AccountCity = "Chandigarh"
+let AccountState; 
+/*
+So basically we have only one way of assigning variable which is using const keyword.
+For variables two ways: var and let but prefer let because var has issue in block scope and functional scope
+*/
+
+// accountId = 12331 This is not allowed as it is a constant
+console.log(accountId);
+ emailId = "thepribisht@sdff1gmail.com"
+ emPassword = "Priyasfeihbs12"
+ AccountCity = "Jaipur"
+//  The values got changed because it is all variables
+console.table([accountId, emailId, emPassword, AccountCity, AccountState]);
