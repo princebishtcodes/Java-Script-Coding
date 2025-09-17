7f0763077199568ad824ce80707181fa04146b71
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-09-17 03:59:04 +0000

    Variables Revision

---
+++ Basics_of_JavaScript/02_variablesTesting.js
@@ -0,0 +1,5 @@
+let AccountName = "Prince"
+const AccountId = 123456
+let AccountCity = "Chandigarh"
+let AccountCountry = "India"
+console.table([AccountName, AccountId,  AccountCity, AccountCountry])
