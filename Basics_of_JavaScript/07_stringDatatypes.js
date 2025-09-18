5da2cef520afa6d975387834b9186b62fb20ad43
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-09-18 17:46:08 +0000

    Strings as Data Types

--- Basics_of_JavaScript/07_stringDatatypes.js
+++ Basics_of_JavaScript/07_stringDatatypes.js
@@ -5,4 +5,4 @@ let emailId = "princebisht@google.com"
 console.log(`My name is ${myName} and my Github Email Id is ${emailId} and my Repo Count is ${repoCount}...`);
 // This is an ideal way doing it
 // This is known as String Impositioning
-
+console.log(Prince);
