84a3d149472d8a0272543670703711e920ec1f62
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-09-22 09:23:52 +0000

    Arrays Basics

--- Basics_of_JavaScript_2/01_arraysinJs.js
+++ Basics_of_JavaScript_2/01_arraysinJs.js
@@ -1,4 +1,9 @@
 //Arrays in JavaScript
 
-myArr = [0,1,2,3,4,5,6];
+const myArr = [0,1,2,3,4,5,6];
 console.log(myArr);
+
+const myHeroes = ["Abhishek", "Rohit", "Neeraj"];
+console.log(myHeroes[1]);
+
+const myArr1 = new Array(1,2,3,4,5,6);
