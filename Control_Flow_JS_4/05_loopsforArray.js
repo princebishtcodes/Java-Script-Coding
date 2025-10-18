639e476b1f2d9cd81072147f74408609c7752743
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-10-18 16:27:43 +0000

    Array Loops like for of, in, each

---
+++ Control_Flow_JS_4/05_loopsforArray.js
@@ -0,0 +1,15 @@
+// Important For Real World Projects
+
+// 1st) for of loop
+// Definition: The for of loop is used to iterate over iterable objects like arrays, strings, maps, sets, etc. It provides a simpler and cleaner syntax compared to traditional loops.
+
+//Syntax
+/*
+for (const element of object) {
+    content here
+}
+*/
+const myHeros = ["Rohit", "Neeraj", "Virat"];
+for (const index of myHeros) {
+    console.log(index);
+}
