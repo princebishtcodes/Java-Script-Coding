7f4e481b032a4c715ac33943da90808649509032
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-10-19 10:26:06 +0000

    for of and for each loop

--- Control_Flow_JS_4/05_loopsforArray.js
+++ Control_Flow_JS_4/05_loopsforArray.js
@@ -45,7 +45,7 @@ const myObj = {
     Name: "Steve Rogers",
     Age: 102,
     Address: "Manhatten, New York, USA",
-    role: "Captain America"
+    Role: "Captain America"
 }
 
 // for (const [keys, values] of myObj) {
@@ -53,3 +53,41 @@ const myObj = {
 // }
 
 // This will give error as Objects are not iterable, so it is not iterable by for of loop, but it can be iterable...
+
+// 2nd) for in loop
+// Definition: The for in loop is used to iterate over the enumerable properties of an object. It is primarily used for objects, but can also be used with arrays (though not recommended for arrays).
+// It is generally used to loop through the properties of an object as the for of loop does not work with objects.
+
+// Syntax
+/*
+for (const key in object) {
+    content here
+}
+*/
+
+for (const key in myObj) {
+    // console.log(`${key} is ${myObj[key]}...`); 
+}
+// Here, key represents the property names of the object, and myObj[key] gives the corresponding values.
+
+// for in loop in Arrays
+
+for (const key in myHeros) {
+    // console.log(key);
+}
+
+// Here, the value is not printed because in for in loop, it iterates over the indices of the array, not the values. To access the values, you would need to use myHeros[key] inside the loop.
+
+for (const key in myHeros){
+    // console.log(myHeros[key]);
+}
+
+// for in loop in Maps
+
+for (const key in map) {
+    // console.log(`${key} : ${map[key]}`); // Printed undefined as map is not iterable by for in loop
+}
+
+// Summary:
+// Use for of loop for iterating over arrays, strings, maps, and sets.
+// Use for in loop for iterating over the properties of objects.
