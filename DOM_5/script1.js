bdb9efe8f23e9df7c54cf52f7539a554290cab8c
Prince Bisht <227972237+princebishtcodes@users.noreply.github.com>
2025-10-23 11:33:33 +0000

    DOM Elements like Parent Child

---
+++ DOM_5/script1.js
@@ -0,0 +1,21 @@
+const Parent = document.querySelector('.parent');
+// console.log(Parent);
+// console.log(Parent.children);
+// console.log(Parent.children[0].innerHTML);
+
+for (let i = 0; i < Parent.children.length; i++) {
+    // console.log((Parent.children[i].innerHTML));
+    
+}
+
+// Parent.children[1].style.color = "orange"
+
+// console.log(Parent.firstElementChild);
+// console.log(Parent.lastElementChild);
+
+// const dayOne = document.querySelector('.day');
+// console.log(dayOne);
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling);
+
+console.log("NODES : ", Parent.childNodes);
